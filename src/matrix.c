3408d6262d84742a22b4f3ea8cb600b09a62dc0f
Martijn Braam <martijn@brixit.nl>
2021-06-13 21:30:53 +0200

    Add debug function to print matrices

--- src/matrix.c
+++ src/matrix.c
@@ -1,3 +1,10 @@
+void print_matrix(float m[9])
+{
+	printf(" [%.2f  %.2f  %.2f] \n", m[0], m[1], m[2]);
+	printf(" [%.2f  %.2f  %.2f] \n", m[3], m[4], m[5]);
+	printf(" [%.2f  %.2f  %.2f] \n\n", m[6], m[7], m[8]);
+}
+
 void
 multiply_matrices(float a[9], float b[9], float out[9])
 {
